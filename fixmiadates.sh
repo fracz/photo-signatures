e165876f666150f2621f649ed86a845518e7d8bb
Wojciech Frącz <fraczwojciech@gmail.com>
2019-09-17 23:33:38 +0200

    Fix MIA dates script

---
+++ fixmiadates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for name in *
+do
+  date="$(echo "$name" | sed -r 's/(IMG|VID)_([0-9]{8})_([0-9]{4})([0-9]{2}).+/\2\3.\4/')"
+  if [[ $date == ????????????.?? ]]
+  then
+    touch -amt ${date} "$name"
+    echo "Updated "$name
+  else
+    echo "Skipped "$name
+  fi
+done
